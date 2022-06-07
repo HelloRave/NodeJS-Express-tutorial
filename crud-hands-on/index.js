50b2a53fb986f7a3f8f45c675580ca7b4e582b9e
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-06-07 03:50:49 +0000

    Completed crud hands on

--- crud-hands-on/index.js
+++ crud-hands-on/index.js
@@ -40,6 +40,38 @@ app.post('/movie/create', async function(req, res){
     res.redirect('/')
 })
 
+app.get('/movie/update/:movie_id', async function(req, res){
+    let movieId = req.params.movie_id;
+    let response = await axios.get(BASE_API_URL + 'movie/' + movieId)
+    let movieDetail = response.data
+    // console.log(response.data)
+    res.render('update-movie', {
+        'movieDetail': movieDetail
+    })
+})
+
+app.post('/movie/update/:movie_id', async function(req, res){
+    await axios.patch(BASE_API_URL + 'movie/' + req.params.movie_id, {
+        'title': req.body.title,
+        'plot': req.body.plot
+    })
+    res.redirect('/')
+})
+
+app.get('/movie/delete/:movie_id', async function(req, res){
+    let movieId = req.params.movie_id; 
+    let response = await axios.get(BASE_API_URL + 'movie/' + movieId)
+    let movieDetails = response.data
+    res.render('delete-movie', {
+        'movieDetails': movieDetails
+    })
+})
+
+app.post('/movie/delete/:movie_id', async function(req, res){
+    await axios.delete(BASE_API_URL + 'movie/' + req.params.movie_id)
+    res.redirect('/')
+})
+
 app.listen(3000, function(){
     console.log('server started')
 })
